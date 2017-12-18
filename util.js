f40c41c6c495198d60d99ea1e6b96c75dbe0278a
Ram Goli <ramsgoli@gmail.com>
2017-12-17 17:23:04 -0800

    fix spacing

--- util.js
+++ util.js
@@ -6,8 +6,7 @@ const verifyGithub = (req) => {
     const theirSig = req.headers['X-Hub-Signature'];
     const payload = JSON.stringify(req.body);
     const secret = PROCESS.ENV.TOKEN;
-    const ourSig = `sha=${crypto.createHmac('sha1', secret). \
-        update(payload).digest('hex')}`;
+    const ourSig = `sha=${crypto.createHmac('sha1', secret)update(payload).digest('hex')}`;
 
     return crypto.timingSafeEqual(Buffer.from(theirSig), Buffer.from(ourSig));
 }
