3f7ff32f541070fdb756b13f2d77731a0128dd60
Ram Goli <ramsgoli@gmail.com>
2017-12-17 17:52:09 -0800

    use try-catch inside crypt buffer generation

--- util.js
+++ util.js
@@ -1,16 +1,24 @@
 const crypto = require('crypto');
 
 const verifyGithub = (req) => {
-    
+    console.log(req.headers);
+    if (!req.headers['x-hub-signature']) {
+        return false;
+    }
     // compare their hmac sig to ours
-    const theirSig = req.headers['X-Hub-Signature'];
+    const theirSig = req.headers['x-hub-signature'];
     const payload = JSON.stringify(req.body);
-    const secret = PROCESS.ENV.TOKEN;
-    const ourSig = `sha=${crypto.createHmac('sha1', secret)update(payload).digest('hex')}`;
+    const secret = process.env.TOKEN;
+    const ourSig = `sha=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
+    console.log(theirSig, ourSig);
 
-    return crypto.timingSafeEqual(Buffer.from(theirSig), Buffer.from(ourSig));
+    try {
+        return crypto.timingSafeEqual(Buffer.from(theirSig), Buffer.from(ourSig));
+    } catch(e) {
+        return false;
+    }
 }
 
-export {
+module.exports = {
     verifyGithub
 };
